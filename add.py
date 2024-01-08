73fa4f518a5c892ab251e935798c5c6bce49e105
VamsiTaneti <147790539+VamsiTaneti@users.noreply.github.com>
2024-01-08 12:34:45 +0530

    Update add.py

--- add.py
+++ add.py
@@ -4,9 +4,9 @@ st.header("Types of Cats")
 col1, col2 = st.columns(2)
 with col1:
   st.subheader("First pic")
-  st.image("./_G0A8343.JPG", caption="FIRST", width=300,use_column_width=True)
+  st.image("./_G0A8343.JPG", caption="1", width=300,use_column_width=True)
   st.write("First pic")
 with col2:
   st.subheader("Second pic")
-  st.image("./_G0A8347.JPG", caption="SECOND", width=300,use_column_width=True)
+  st.image("./_G0A8347.JPG", caption="2", width=300,use_column_width=True)
   st.write("Second pic")
