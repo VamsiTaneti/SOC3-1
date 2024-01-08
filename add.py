4df6fc99e1019bdfe4ad0eeb949858f89c4d4373
VamsiTaneti <147790539+VamsiTaneti@users.noreply.github.com>
2024-01-08 12:36:34 +0530

    Update add.py

--- add.py
+++ add.py
@@ -1,12 +1,11 @@
 import streamlit as st
-st.set_page_config(page_title='Cats')
-st.header("Types of Cats")
+st.set_page_config(page_title='Pics')
+st.header("My pictures")
 col1, col2 = st.columns(2)
 with col1:
   st.subheader("First pic")
   st.image("./_G0A8343.JPG", caption="1", width=300,use_column_width=True)
-  st.write("First pic")
 with col2:
   st.subheader("Second pic")
   st.image("./_G0A8347.JPG", caption="2", width=300,use_column_width=True)
-  st.write("Second pic")
+st.header("Favourate pics")
